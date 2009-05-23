084c4872cf4f567d56bf53d47fcfa3a8e4a1246d
till <till@php.net>
2009-05-23 10:35:41 -0500

    added some description, etc.

--- examples/places.php
+++ examples/places.php
@@ -1,15 +1,31 @@
 <?php
+/**
+ * config file (contains key and secret)
+ * @ignore
+ */
 require_once dirname(__FILE__) . '/conf.php';
+
+/**
+ * Services_Qype
+ * @ignore
+ */
 require_once dirname(__FILE__) . '/../Services/Qype.php';
+
+/**
+ * include external oauth library
+ * @ignore
+ */
 require_once dirname(__FILE__) . '/../external/OAuth.php';
 
+// set longitude and latitude for query
+$near = false;
 $lat  = 52.50743;
 $long = 13.467888;
 
 try {
     $qype = new Services_Qype($appKey, $appSecret);
-    $qype->places()->setGeoLocation($long, $lat);
-    $qype->places()->getPlaces(true);
+    //$qype->places()->setGeoLocation($long, $lat);
+    var_dump($qype->places()->getPlaces($near));
 } catch (Exception $e) {
     var_dump($e);
 }
