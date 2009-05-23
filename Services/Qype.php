15670ebdef7bf7d2cad58e49a1865a11002a797b
Till Klampaeckel <till@php.net>
2009-05-23 21:12:34 +0200

    support for languages and other sub methods

--- Services/Qype.php
+++ Services/Qype.php
@@ -52,6 +52,11 @@ class Services_Qype
     protected $client;
     protected $consumer;
     
+    protected $language = array(
+        'content' => 'en',
+        'ui'      => 'en_GB',
+    );
+    
     /**
      * CTR
      * 
@@ -78,6 +83,49 @@ class Services_Qype
         $this->version = $version;
         return $this;
     }
+
+    /**
+     * Usually, it's plenty to set the UI language.
+     *
+     * Set this if you want (e.g.) a Germany UI, and reviews in English.
+     *
+     * @param string $lang The language.
+     *
+     * @return Services_Qype
+     * @throws Services_Qype_Exception In case of an unsupported language.
+     */
+    public function setContentLanguage($lang)
+    {
+        static $languages = array('de', 'en', 'fr', 'es', 'pt');
+
+        if (!in_array($lang, $languages)) {
+            throw new Services_Qype_Exception("Unsupported language: {$lang}.");
+        }
+        $this->language['content'] = $lang;
+        return $this;
+    }
+
+    /**
+     * UI language, is the language of the user interface. In differen to UI, on
+     * Qype, there's also the content language.
+     * (See {@link self::setContentLanguage()}).
+     *
+     * @param string $locale The language.
+     *
+     * @return Services_Qype
+     * @throws Services_Qype_Exception
+     */
+    public function setUiLanguage($locale)
+    {
+        static $locales = array('de_DE', 'en_GB', 'fr_FR', 'es_ES', 'pt_BR');
+
+        if (!in_array($locale, $locales)) {
+            throw new Services_Qype_Exception("Unsupported locale: {$locale}");
+        }
+
+        $this->language['ui'] = $locale;
+        return $this;
+    }
     
     /**
      * __call
@@ -89,7 +137,7 @@ class Services_Qype
      */
     public function __call($method, $params)
     {
-        static $driver = array('locator', 'places', 'users');
+        static $driver = array('categories', 'locator', 'places', 'reviews', 'tags', 'users');
         if (!in_array($method, $driver)) {
             throw new Services_Qype_Exception("Unknown feature: {$method}");
         }
@@ -97,7 +145,10 @@ class Services_Qype
 
             $className = "Services_Qype_" . ucwords($method);
             
-            $this->stack[$method] = new $className($this->version);
+            $this->stack[$method] = new $className(
+                $this->language,
+                $this->version
+            );
             $this->stack[$method]->setConsumer($this->consumer);
             if (($this->client instanceof HTTP_Request2)) {
                 $this->stack[$method]->setClient($this->client);
