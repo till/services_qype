521e27f6b02e7ef1aa357fda392752f8b6521cae
Till Klampaeckel <till@php.net>
2009-05-23 21:13:28 +0200

    example for tags

---
+++ examples/tags.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * config file (contains key and secret)
+ * @ignore
+ */
+require_once dirname(__FILE__) . '/conf.php';
+
+/**
+ * Services_Qype
+ * @ignore
+ */
+require_once dirname(__FILE__) . '/../Services/Qype.php';
+
+/**
+ * include external oauth library
+ * @ignore
+ */
+require_once dirname(__FILE__) . '/../external/OAuth.php';
+
+try {
+    $qype = new Services_Qype($appKey, $appSecret);
+
+    //var_dump($qype->tags()->getTag('cheap'));
+    var_dump($qype->tags()->getReviews('cheap'));
+
+} catch (Exception $e) {
+    var_dump($e);
+}
